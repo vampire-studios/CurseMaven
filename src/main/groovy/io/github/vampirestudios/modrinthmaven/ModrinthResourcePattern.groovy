5b0026d43d5baacedad69982e4c0f9de6b0f9295
Cat Core <arthur.bambou@gmail.com>
2020-11-08 01:29:45 +0100

    Fix error in console

--- src/main/groovy/io/github/vampirestudios/modrinthmaven/ModrinthResourcePattern.groovy
+++ src/main/groovy/io/github/vampirestudios/modrinthmaven/ModrinthResourcePattern.groovy
@@ -14,7 +14,7 @@ import com.google.gson.*
 class ModrinthResourcePattern extends M2ResourcePattern {
 
     /**
-     * Cache that's used to store the result of {@link #getExtension(java.lang.String)}
+     * Cache that's used to store the result of {@link #getExtension(java.lang.String, java.lang.String)}
      */
     static final Map<String, String> EXTENSION_CACHE = new HashMap<>()
 
@@ -50,11 +50,11 @@ class ModrinthResourcePattern extends M2ResourcePattern {
 
     /**
      * Gets the suffix for {@link #DOWNLOAD_URL}. Used to resolve the URL maven patterns. NOTE: ANY EXCEPTION THROWN BY THIS WILL JUST BE CONSUMED
-     * @param versionId the project slug if {@code group} is `modrinth.maven`, or the project id if it's `modrinth.maven.id`
+     * @param modId the project id
+     * @param versionId the version id
      * @return the extension for the given artifacts.
      */
     static Optional<String> getExtension(String modId, String versionId) {
-        println("Extension: $modId $versionId")
 
         //Gets the cache key for this object. the classifier can be null, hence why Objects.toString is used.
         def cacheKey = "$modId:$versionId"
@@ -66,7 +66,6 @@ class ModrinthResourcePattern extends M2ResourcePattern {
         }
 
         def modJson = new InputStreamReader(new URL("https://api.modrinth.com/api/v1/mod/$modId").openStream())
-        println(modJson)
         
         JsonObject modJsonObject = GSON.fromJson(modJson, JsonObject.class)
         JsonArray fileList = modJsonObject.getAsJsonArray("versions")
@@ -77,50 +76,15 @@ class ModrinthResourcePattern extends M2ResourcePattern {
             }
         })
 
-        if (!contains) {
+        if (!contains && !versionId.contains("@")) {
             throw new IllegalArgumentException("Can't find Version ID in Version List of Mod ID. VersionId: $versionId, ModId: $modId")
         }
 
         def fileJson = new InputStreamReader(new URL("https://api.modrinth.com/api/v1/version/$versionId").openStream())
-        println(fileJson)
         JsonObject fileJsonObject = GSON.fromJson(fileJson, JsonObject.class)
         JsonObject fileInfo = fileJsonObject.getAsJsonArray("files").get(0) as JsonObject
-        //Get the normal jar result. This should never be empty.
+        
         def result = fileInfo.get("url").getAsString()
-//        println(url)
-//        def result = new URL(url).text
-//        if(result.isEmpty()) {
-//            throw new IllegalArgumentException("Version ID is invalid. VersionId: '$versionId'")
-//        }
-
-//        //If we need to search for a classifier, then do so
-//        if(classifier != null) {
-//            //Get the normal, no classifier jar name, and the file id for that jar name
-//            def jarName = result.substring(result.lastIndexOf('/'), result.length() - ".jar".length())
-//            def start = Integer.parseInt(fileID)
-//            boolean found = false
-//
-//            //Go from the current file version to 20 + the current file version.
-//            //For each version, get the download url and see if it matches with the found jar name, along with the -classifier prefix.
-//            //If so then set the result to that and mark the classifier as found
-//            for(int i = 1; i <= 20; i++) {
-//                try {
-//                    def tryResult = new URL("https://addons-ecs.forgesvc.net/api/v2/addon/0/file/${start + i}/download-url").text
-//                    if(tryResult.endsWith("$jarName-${classifier}.jar")) {
-//                        result = tryResult
-//                        found = true
-//                        break
-//                    }
-//                } catch(FileNotFoundException ignored) {
-//                    break
-//                }
-//            }
-//
-//            //Classifier could not be found, this is fine.
-//            if(!found) {
-//                return Optional.empty()
-//            }
-//        }
 
         EXTENSION_CACHE.put(cacheKey, result)
         Optional.ofNullable(result)
